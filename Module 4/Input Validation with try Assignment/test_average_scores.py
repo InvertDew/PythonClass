4ccdeeaf49e41befd34688d021947db3bd9b1849
InvertDew <justinleeoglesby@gmail.com>
2020-06-10 23:43:09 -0500

    second fail

--- Module 4/Input Validation with try Assignment/test_average_scores.py
+++ Module 4/Input Validation with try Assignment/test_average_scores.py
@@ -5,6 +5,8 @@ class FunctionTestCase(unittest.TestCase):
     def test_average_exception(self):
         with self.assertRaises(ValueError):
             average(-90,80, 78)
+        with self.assertRaises(ValueError):
+            average(90,-80, 78)
 
 if __name__ == '__main__':
     test_average_exception()
