3fa965a5affe823401dd66e3b1748e2f703df607
InvertDew <justinleeoglesby@gmail.com>
2020-06-10 21:47:38 -0500

    test_price_under_between_thirty_fifty tests passing

--- Module 4/test_cost.py
+++ Module 4/test_cost.py
@@ -9,13 +9,18 @@ class FunctionTestCase(unittest.TestCase):
         self.assertTrue(10.00 > calculate_price(8, 5, 20)),
         self.assertTrue(10.00 > calculate_price(8, 10, 10)),
 
-    def test_price_under_between_thirty_fifty(self):
-        print(calculate_price(10, 5, 10))
+    def test_price_under_between_ten_thirty(self):
         self.assertTrue(10 <= calculate_price(10, 5, 10) < 30)
         self.assertTrue(10 <= calculate_price(15, 10, 15) < 30)
         self.assertTrue(10 <= calculate_price(20, 10, 20) < 30)
         self.assertTrue(10 <= calculate_price(29, 10, 10) < 30)
 
+    def test_price_under_between_thirty_fifty(self):
+        self.assertTrue(30 <= calculate_price(30, 5, 10) < 50)
+        self.assertTrue(30 <= calculate_price(35, 10, 15) < 50)
+        self.assertTrue(30 <= calculate_price(45, 10, 20) < 50)
+
 if __name__ == '__main__':
     test_price_under_ten()
+    test_price_under_between_ten_thirty()
     test_price_under_between_thirty_fifty()
