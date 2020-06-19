dea58a5c77c8d8c8308beaf5572cd1605660843b
InvertDew <justinleeoglesby@gmail.com>
2020-06-19 00:52:04 -0500

    test_score_input_test_name passing

--- Module 6/test_valid_input_in_functions.py
+++ Module 6/test_valid_input_in_functions.py
@@ -4,7 +4,8 @@ from validate_input_in_functions import score_input
 
 class FunctionTestCase(unittest.TestCase):
     def test_score_input_test_name(self):
-        self.assertTrue()
+        self.assertTrue(score_input("hello") == {'hello': 0})
+        self.assertTrue(score_input("fred") == {'fred': 0})
 
         
 if __name__ == '__main__':
--- Module 6/validate_input_in_functions.py
+++ Module 6/validate_input_in_functions.py
@@ -10,12 +10,17 @@ Last date modified: 6/18/2020
 
 def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
     """
-    :param message: incoming string
-    :param n: number of times to duplicate message
-    :returns: message concatanted to itself n times
+    :param test_name: incoming string
+    :param n: test_score: optional integer representing test score
+    :param test_score optional invalid message
+    :returns: object with test_name and test_score
     """
-    pass
+    if test_score < 0 or test_score > 100:
+        return invalid_message
+    print('Test name:', test_name)
+    return { test_name: test_score }
 
 
 if __name__ == '__main__':
+    print(score_input("test"))
     time.sleep(30)
