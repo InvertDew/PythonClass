4fd4e86a4ada30514a2ad74d70862c643c0cc192
InvertDew <justinleeoglesby@gmail.com>
2020-06-19 00:41:14 -0500

    Got function and unit tests set up

---
+++ Module 6/test_valid_input_in_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from validate_input_in_functions import score_input
+
+
+class FunctionTestCase(unittest.TestCase):
+    def test_score_input_test_name(self):
+        self.assertTrue()
+
+        
+if __name__ == '__main__':
+    test_score_input_test_name()
---
+++ Module 6/validate_input_in_functions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import time
+
+"""
+Program: validate_input_in_functions.py
+Author: Justin Oglesby
+Last date modified: 6/18/2020
+"""
+
+
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+    """
+    :param message: incoming string
+    :param n: number of times to duplicate message
+    :returns: message concatanted to itself n times
+    """
+    pass
+
+
+if __name__ == '__main__':
+    time.sleep(30)
