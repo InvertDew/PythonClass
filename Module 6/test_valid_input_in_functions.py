f2e6b0ed6e5a8d0f2330abef9598324d70a69ab0
InvertDew <justinleeoglesby@gmail.com>
2020-06-19 00:53:59 -0500

    test_score_input_ test_score_valid passing

--- Module 6/test_valid_input_in_functions.py
+++ Module 6/test_valid_input_in_functions.py
@@ -7,6 +7,11 @@ class FunctionTestCase(unittest.TestCase):
         self.assertTrue(score_input("hello") == {'hello': 0})
         self.assertTrue(score_input("fred") == {'fred': 0})
 
+    def test_score_input_test_score_valid(self):
+        self.assertTrue(score_input("hello", 33) == {'hello': 33})
+        self.assertTrue(score_input("fred", 77) == {'fred': 77})
+
         
 if __name__ == '__main__':
     test_score_input_test_name()
+    test_score_input_test_score_valid()
